111ffe90cf32fdf7accf65afefa5101a291c5162
TruongNgan6 <truongvokimngan13@gmail.com>
2023-11-27 10:14:17 +0700

    newDestination

---
+++ src/Components/Destination/Asian/Asian.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Asian.css";
+
+const Asian = () => {
+  return (
+    <div>
+      <div>This Asian page</div>
+    </div>
+  );
+};
+
+export default Asian;
--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Destination.css";
+import destination01 from "../../../src/Assets/Images/destination01.jpg";
 
 const Destination = () => {
   return (
     <div>
-      <div>This Destination page</div>
+      <div className="destination-content">
+        <div className="destination-component">
+          <img src={destination01}></img>
+          <div className="quantity-tours">5 tours</div>
+          <div className="overlay-tour">
+            <dvi className="destination-text">
+              <h2>Asia</h2>
+              <p>
+                Far far away, behind the word mountains, far from the countries
+                Vokalia and Consonantia, there live the blind texts. Separate.
+              </p>
+              <a href="#">View all tours</a>
+            </dvi>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
